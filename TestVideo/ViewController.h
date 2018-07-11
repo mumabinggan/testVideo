205c56e127c7537e358822f88036e26ae55326c4
zhongan <zhongan@zhongandembp-2.lan>
2018-07-12 07:19:23 +0800

    Initial Commit

---
+++ TestVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestVideo
+//
+//  Created by zhongan on 2018/7/12.
+//  Copyright © 2018年 sss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestVideo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestVideo
+//
+//  Created by zhongan on 2018/7/12.
+//  Copyright © 2018年 sss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
